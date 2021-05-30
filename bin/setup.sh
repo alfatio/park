41d69957c1e839dcf2bb77cf8ac13b9dd83c778b
alfatio <alfatioutama@gmail.com>
2021-05-30 07:00:57 +0000

    add_setup_script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
 # Add script to:
 # * Install dependencies
 # * Build/Compile
 # * Run Test Suit to validate
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR/../parking_lot && npm install && npm test
 #
 # After this is run, bin/parking_lot
 # should Just Work.
